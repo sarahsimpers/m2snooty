c78a11685b615d0a73c550b48f7a0ebe3bb4ccf1
Sarah Simpers <sarah.simpers@mongodb.com>
2023-06-02 08:54:22 -0400

    Changes name of scripts and files to m2snooty

--- m2snooty-script.sh
+++ m2snooty-script.sh
@@ -7,13 +7,13 @@ do
     esac
 done
 
-python3 ~/$projects/m2r/m2r.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/concepts.txt
+python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/concepts.txt
 mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/concepts.rst ~/$projects/cloud-docs/source/sdk/go/concepts.txt
-python3 ~/$projects/m2r/m2r.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/authentication.txt
+python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/authentication.txt
 mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/authentication.rst ~/$projects/cloud-docs/source/sdk/go/authentication.txt
-python3 ~/$projects/m2r/m2r.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/error_handling.txt
+python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/error_handling.txt
 mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/error_handling.rst ~/$projects/cloud-docs/source/sdk/go/error_handling.txt
-python3 ~/$projects/m2r/m2r.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/migration.txt
+python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/migration.txt
 mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/migration.rst ~/$projects/cloud-docs/source/sdk/go/migration.txt
-python3 ~/$projects/m2r/m2r.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/reference.txt
+python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/reference.txt
 mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/reference.rst ~/$projects/cloud-docs/source/sdk/go/reference.txt

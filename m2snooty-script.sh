0dc675f71ff3a5703fba0f7e59fbe3fa40e5b6eb
Sarah Simpers <sarah.simpers@mongodb.com>
2023-06-02 10:55:54 -0400

    Adds for loop to avoid hardcoding the files

--- m2snooty-script.sh
+++ m2snooty-script.sh
@@ -7,13 +7,23 @@ do
     esac
 done
 
-python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/concepts.txt
-mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/concepts.rst ~/$projects/cloud-docs/source/sdk/go/concepts.txt
-python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/authentication.txt
-mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/authentication.rst ~/$projects/cloud-docs/source/sdk/go/authentication.txt
-python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/error_handling.txt
-mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/error_handling.rst ~/$projects/cloud-docs/source/sdk/go/error_handling.txt
-python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/migration.txt
-mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/migration.rst ~/$projects/cloud-docs/source/sdk/go/migration.txt
-python3 ~/$projects/m2snooty/m2snooty.py ~/$projects/cloud-docs/source/includes/sdk/go/markdown/reference.txt
-mv ~/$projects/cloud-docs/source/includes/sdk/go/markdown/reference.rst ~/$projects/cloud-docs/source/sdk/go/reference.txt
+FILES=~/$projects/cloud-docs/source/includes/sdk/go/markdown/*
+for f in $FILES
+do
+  echo "Transforming $f to Snooty-friendly rST..."
+  python3 ~/$projects/m2snooty/m2snooty.py $f
+  # take action on each file. $f store current file name
+done
+
+RSTFILES=~/$projects/cloud-docs/source/includes/sdk/go/markdown/*.rst
+for f in $RSTFILES
+do
+  echo "Moving $f"
+  mv $f ~/$projects/cloud-docs/source/sdk/go/
+done
+
+MOVEDRSTFILES=~/$projects/cloud-docs/source/sdk/go/*.rst
+for f in $MOVEDRSTFILES
+do  
+  mv -- "$f" "${f%.rst}.txt"
+done
